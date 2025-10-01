92064f47268043ea930f998678e52e518d7b47d0
johannax94 <jmeguedad1@myges.fr>
2025-10-01 17:17:52 +0200

    hihi

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,54 +1,46 @@
 section .data
-    msg db 'Entrez un chiffre', 0xA
-    len equ $ - msg
-    not_42_msg db '1', 0xA
-    is_42_msg db '1337', 0xA
-    buffer db 0, 0
+    good db "42", 0x0A
+    goodlen equ $ - good
+
+    msg db "1337", 0x0A
+    msglen equ $ - msg
+
+section .bss
+    buf resb 8
 
 section .text
+    global _start
 
-
-    global _start   
 _start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buf
+    mov rdx, 8
+    syscall
+    mov rbx, rax
 
+    mov rcx, goodlen
+    cmp rbx, rcx
+    jne _fail
+
+    mov rsi, buf
+    mov rdi, good
+    mov rcx, goodlen
+    repe cmpsb
+    jne _fail
+
+_success:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, len
+    mov rdx, msglen
     syscall
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, buffer
-    mov rdx, 2
-    syscall
-
-    mov al, byte [buffer]
-    cmp al, '4'
-    jne not_42
-    mov al, byte [buffer + 1]
-    cmp al, '2'
-    jne not_42
-    jmp is_42
-
-    not_42:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, not_42_msg
-    mov rdx, 2
-    syscall
-    jmp exit
-
-    is_42:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, is_42_msg
-    mov rdx, 4
-    syscall
-    jmp exit
-
-
-    exit:
-    mov rax, 60
     xor rdi, rdi
+    mov rax, 60
+    syscall
+
+_fail:
+    mov rdi, 1
+    mov rax, 60
     syscall
